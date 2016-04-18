c3394bb7e8275866c78e00831165536653f38701
ironjellyfish <tewilson3d@gmail.com>
2016-04-17 18:20:57 -0700

    added enemy grunt added nav mesh for te test level

--- UnitySmashTVGC/Assets/Scripts/NPCGruntMovement.cs
+++ UnitySmashTVGC/Assets/Scripts/NPCGruntMovement.cs
@@ -6,17 +6,22 @@ public class NPCGruntMovement : MonoBehaviour
 
     // Target
     public GameObject target = null;
-    public Vector3 aimVector;
+    //public Vector3 aimVector;
 
-    // Enemy Movement
-    public float speed = 3f;
-    Vector3 movement;
-    Rigidbody enemyRigidBody;
+    //// Enemy Movement
+    //public float speed = 3f;
+    //Vector3 movement;
+    //Rigidbody enemyRigidBody;
+
+    //Transform player;
+    NavMeshAgent nav;
 
     // Use this for initialization  
     void Awake()
     {
-        enemyRigidBody = GetComponent<Rigidbody>();
+        //enemyRigidBody = GetComponent<Rigidbody>();
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
     }
 
     void Start ()
@@ -24,20 +29,26 @@ public class NPCGruntMovement : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player");
 	}
 	
-    void FixedUpdate()
+    void Update()
     {
-        float h = target.transform.position.x;
-        float v = target.transform.position.z;
-
-        // Move the enemy
-        Move(h, v);
+        //movement = movement.normalized * speed * Time.deltaTime;
+        nav.SetDestination(target.transform.position);
     }
 
-    void Move(float h, float v)
-    {
-        movement.Set(h, 0f, v);
-        movement = movement.normalized * speed * Time.deltaTime;
-        enemyRigidBody.MovePosition(transform.position + movement);
-    }
+    //void FixedUpdate()
+    //{
+    //    float h = target.transform.position.x;
+    //    float v = target.transform.position.z;
+
+    //    // Move the enemy
+    //    Move(h, v);
+    //}
+
+    //void Move(float h, float v)
+    //{
+    //    movement.Set(h, 0f, v);
+    //    movement = movement.normalized * speed * Time.deltaTime;
+    //    enemyRigidBody.MovePosition(transform.position + movement);
+    //}
 
 }
